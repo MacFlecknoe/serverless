c9bd89ed921b87689b1bf4df46aa53c94063e134
Lambert <michaellambert@quickenloans.com>
2018-07-09 09:49:47 -0400

    initial commit

---
+++ website/24-hour-video/js/config.js
@@ -0,0 +1,8 @@
+﻿var configConstants = {
+    auth0: {
+        domain: 'michaellambert.auth0.com',
+        clientId: 'CtiLvQ4H7cHG7ZUIq9qIZi7WLxKYrhMv',
+        scope: 'openid email profile'
+    },
+    apiBaseUrl: 'https://API-GATEWAY-URL/dev'
+};
---
+++ website/24-hour-video/js/user-controller.js
@@ -0,0 +1,103 @@
+﻿var userController = {
+    data: {
+        auth0Lock: null,
+        config: null
+    },
+    uiElements: {
+        loginButton: null,
+        logoutButton: null,
+        profileButton: null,
+        profileNameLabel: null,
+        profileImage: null
+    },
+    init: function (config) {
+
+        var that = this;
+
+        this.uiElements.loginButton = $('#auth0-login');
+        this.uiElements.logoutButton = $('#auth0-logout');
+        this.uiElements.profileButton = $('#user-profile');
+        this.uiElements.profileNameLabel = $('#profilename');
+        this.uiElements.profileImage = $('#profilepicture');
+
+        this.data.config = config;
+
+        this.data.auth0Lock = new Auth0Lock(config.auth0.clientId, config.auth0.domain, {
+            auth: {
+                params: {
+                    scope: config.auth0.scope
+                }
+            }
+        }); // params set in config.js
+        this.data.auth0Lock.on("authenticated", function (authResult) {
+            console.log("authenticated");
+            that.retrieveProfileData(authResult.accessToken);
+            localStorage.setItem('userToken', authResult.accessToken);
+        });
+
+        var idToken = localStorage.getItem('userToken');
+
+        if (idToken) {
+            this.retrieveProfileData(idToken);
+        }
+        this.wireEvents();
+    },
+    retrieveProfileData: function (accessToken) {
+
+        var that = this;
+
+        console.log("retrieving profile");
+        this.configureAuthenticatedRequests();
+        this.data.auth0Lock.getUserInfo(accessToken, function (err, profile) {
+            if (err) {
+                return alert('There was an error getting the profile: ' + err.message);
+            }
+            that.showUserAuthenticationDetails(profile);
+        });
+    },
+    configureAuthenticatedRequests: function () {
+        $.ajaxSetup({
+            'beforeSend': function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('userToken'));
+            }
+        });
+    },
+    showUserAuthenticationDetails: function (profile) {
+
+        var showAuthenticationElements = !!profile; //coerce into a boolean (!!1 evalutes to true, !!0 evalutes to false)
+
+        if (showAuthenticationElements) {
+            this.uiElements.profileNameLabel.text(profile.nickname);
+            this.uiElements.profileImage.attr('src', profile.picture);
+        }
+        this.uiElements.loginButton.toggle(!showAuthenticationElements);
+        this.uiElements.logoutButton.toggle(showAuthenticationElements);
+        this.uiElements.profileButton.toggle(showAuthenticationElements);
+    },
+    wireEvents: function () {
+
+        var that = this;
+
+        this.uiElements.loginButton.click(function (e) {
+            console.log("show");
+            that.data.auth0Lock.show();
+        });
+        this.uiElements.logoutButton.click(function (e) {
+
+            localStorage.removeItem('userToken');
+
+            that.uiElements.logoutButton.hide();
+            that.uiElements.profileButton.hide();
+            that.uiElements.loginButton.show();
+        });
+        this.uiElements.profileButton.click(function (e) {
+
+            var url = that.data.config.apiBaseUrl + '/user-profile';
+
+            $.get(url, function (data, status) {
+                $('#user-profile-raw-json').text(JSON.stringify(data, null, 2));
+                $('#user-profile-modal').modal();
+            })
+        });
+    }
+}

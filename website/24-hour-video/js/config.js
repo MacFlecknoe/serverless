40ab5bb11b68e8d2615b83afdb513388b5262bb7
Michael Lambert <MacFlecknoe@users.noreply.github.com>
2018-07-22 22:06:03 -0400

    formatting

--- website/24-hour-video/js/config.js
+++ website/24-hour-video/js/config.js
@@ -3,7 +3,7 @@
         domain: 'michaellambert.auth0.com',
         clientId: 'CtiLvQ4H7cHG7ZUIq9qIZi7WLxKYrhMv',
         scope: 'openid email profile',
-		audience: 'https://michaellambert.auth0.com/api/v2/'
+	audience: 'https://michaellambert.auth0.com/api/v2/'
     },
     apiBaseUrl: 'https://solmoicae5.execute-api.us-east-1.amazonaws.com/dev'
 };
